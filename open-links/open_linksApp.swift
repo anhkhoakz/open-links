411d7dcd7dc2a27085464b768cccd2b6ca223d26
Nguyễn Huỳnh Anh Khoa <noreply-git@anhkhoakz.simplelogin.com>
2025-07-28 20:19:03 +0700

    Initial Commit

---
+++ open-links/open_linksApp.swift
@@ -0,0 +1,17 @@
+//
+//  open_linksApp.swift
+//  open-links
+//
+//  Created by Nguyen Huynh Anh Khoa on 28/7/25.
+//
+
+import SwiftUI
+
+@main
+struct open_linksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
